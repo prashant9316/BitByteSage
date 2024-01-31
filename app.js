fd3dbd0f7e7589a6c388c1f0ef2dea30389707af
prashant9316 <pgmd.9873@gmail.com>
2024-01-31 22:26:37 +0530

    updated a lot of things

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const resourcesRoues = require('./routes/resources-routes');
 
 app.use('/', frontendRoutes);
 app.use('/services', servicesRoutes);
-app.use('/resources', resourcesRoues);
+app.use('/trainings', resourcesRoues);
 
 app.use((err, req, res, next) => {
     let error = { ...err }
@@ -70,4 +70,4 @@ app.use((err, req, res, next) => {
 });
 
 const http = require("http").createServer(app);
-http.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+http.listen(process.env.PORT, () => console.log(`Open Website @: http://localhost:${process.env.PORT}`));
--- routes/resources-routes.js
+++ routes/resources-routes.js
@@ -1,5 +1,7 @@
 const routes = require('express').Router()
 
-
+routes.get('/', (req, res) => {
+    res.render('frontend/resources/index', {layout: false});
+})
 
 module.exports = routes;
